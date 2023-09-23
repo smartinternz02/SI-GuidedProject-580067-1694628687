e1240b810959f83addc76f05b72908c40a9104fa
dsk-ksd <89018695+dsk-ksd@users.noreply.github.com>
2023-09-23 11:46:28 +0530

    Add files via upload

---
+++ 1/app/src/main/java/com/example/a1/MainActivity.kt
@@ -0,0 +1,92 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.a1
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a1.ui.theme._1Theme
+import kotlin.math.sin
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            _1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginPage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginPage() {
+
+    var email: String by remember { mutableStateOf("") }
+    var password: String by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    Column {
+        
+        Text(
+            text = "Email",
+            modifier = Modifier.padding(10.dp)
+            )
+        
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+
+        Text(
+            text = "Password",
+            modifier = Modifier.padding(10.dp)
+        )
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+
+        Button(onClick = {
+            Toast.makeText(
+                context,
+                "You have logged in",
+                Toast.LENGTH_LONG)
+                .show()
+        }) {
+
+            Text(text = "Login")
+        }
+    }
+}
